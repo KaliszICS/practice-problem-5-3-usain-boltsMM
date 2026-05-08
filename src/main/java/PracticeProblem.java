77ac846998b75f8296f943ce8000fdb7bad8cf46
usain-boltsMM <343421046@tdsb.ca>
2026-05-08 21:32:48 +0000

    umbra

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,50 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-	}
+    }
 
+    public static String[] addString(String[] arr, String str, int index) {
+        String[] newArr = new String[arr.length + 1];
+        int oldIndex = 0;
+
+        for (int i = 0; i < newArr.length; i++) {
+            
+			if (i == index) {
+                newArr[i] = str;
+            } 
+				else {
+                	newArr[i] = arr[oldIndex];
+                	oldIndex++;
+            }
+        }
+
+        return newArr;
+    }
+
+    public static char[] removeChars(char[] arr, char character) {
+        
+		int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            
+			if (arr[i] == character) {
+                count++;
+            }
+        }
+
+        char[] newArr = new char[arr.length - count];
+        
+		int newIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            
+			if (arr[i] != character) {
+                newArr[newIndex] = arr[i];
+                newIndex++;
+            }
+        }
+
+        return newArr;
+    }
 }
